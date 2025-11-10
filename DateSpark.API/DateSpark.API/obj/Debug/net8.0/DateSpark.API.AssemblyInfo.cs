62ca94febe81240ad082fa712150708ea0551bb9
Nikolaenko Sergey <nikolaenkoserzh@gmail.com>
2025-11-11 00:06:01 +0800

    Fix Dockerfile name and content

--- DateSpark.API/DateSpark.API/obj/Debug/net8.0/DateSpark.API.AssemblyInfo.cs
+++ DateSpark.API/DateSpark.API/obj/Debug/net8.0/DateSpark.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DateSpark.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be652e79673201f5f5e5c0e1a68bc92f0b13e6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ddac7e186f24920e44d83eb3099b627ab2790c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DateSpark.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DateSpark.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
